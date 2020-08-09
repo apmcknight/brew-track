abfa6b8bc814e281c626206de4d24a65a27e59e8
Adam McKnight <adammcknight@me.com>
2020-08-09 17:39:57 -0400

    Added progress selector, and updated the SQLite db instacne.

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -4,6 +4,7 @@ namespace brew_track.Models
     public class Orders
     {
         public int ID { get; set; } // Auto-pop
+        public string Status { get; set; }
         public string FirstName { get; set; } // Input Field
         public string Items { get; set; } // Add Value from List, like a to-do list application.
         public string TableNumber { get; set; } // Input Value
